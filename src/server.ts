211d9c1f2c400dd124cefd1966daeaff35f4441f
rafin8990 <rafin8990@gmail.com>
2023-05-29 13:03:55 +0600

    [done] setup express mongoose cors purser

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+import config from './config/index'
+import app from "./app";
+
+async function run() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log("Database Is connected successfully");
+        app.listen(config.port, () => {
+            console.log(`application listening on port ${config.port}`)
+        })
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+run().catch(error => console.log(error))
+
