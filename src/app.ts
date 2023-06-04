55e9ec755bc36b142c737d7fc5401a89545ea2c8
rafin8990 <rafin8990@gmail.com>
2023-06-04 11:24:36 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,13 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app:Application = express()
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('university server is running carefully')
 })
 
-
-
-export default app;
+export default app
